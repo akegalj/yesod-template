24f6161dd8e64632543ee5e4edb4b294bf7b21ce
Ante Kegalj <akegalj@gmail.com>
2023-07-29 15:30:50 +0200

    Add yesod

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
-module Main where
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes| / HomeR GET |]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = warp 3000 HelloWorld
