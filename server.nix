500d5ef9df8d702d1030049b2d6b0d3da7ca65d4
Ante Kegalj <akegalj@gmail.com>
2023-09-07 11:39:35 +0200

    Fix typo

--- server.nix
+++ server.nix
@@ -10,12 +10,12 @@ in
 
   # Use nginx or keter in production
   systemd.services."${projectName}" = {
-    description = "Yesod ${projectName }template";
+    description = "Yesod ${projectName} template";
     script = let app = import (./.); in "${app}/bin/${projectName}";
     wantedBy = ["multi-user.target" "ngingx.service" ];
     serviceConfig = {
       User = "${projectName}";
-      Group = "${projectname}";
+      Group = "${projectName}";
       # AmbientCapabilities = "CAP_NET_BIND_SERVICE";
       Restart = "always";
       RestartSec = "10s";
