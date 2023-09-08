46dc20e5bb7634f04474884380fae0d550c128ae
Ante Kegalj <akegalj@gmail.com>
2023-09-08 17:20:36 +0200

    Update bootstrap

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -155,7 +155,7 @@ instance Yesod App where
     -- you to use normal widget features in default-layout.
 
     pc <- widgetToPageContent $ do
-      addStylesheet $ StaticR css_bootstrap_css
+      -- addStylesheet $ StaticR css_bootstrap_css
       -- \^ generated from @Settings/StaticFiles.hs@
       $(widgetFile "default-layout")
     withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
